165f7b8b19890dfaae00be9305ab82976865d932
masqaramuerte-syn <jim@syncromsp.com>
2021-02-04 13:42:37 -0800

    small cleanup on routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   post "/api/deploy_log", to: "api#deploy_log"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "servers#index"
+
   resources :slack do
     collection do
       post :staging
       post :slash_super_staging
       post :super_staging_event
       post :super_staging_interactivity
-    end	
+    end
   end
 end
